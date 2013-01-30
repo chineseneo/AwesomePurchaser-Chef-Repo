a8ec57fbb5d6552e6709b7a0cc30e420d0df92eb
Longkai Cao <lkcao@thoughtworks.com>
2013-01-30 18:07:19 +0800

    add chef-solo execution to install.sh. And...install.sh should be under root

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@ chef_binary=`which chef-solo`
 knife_binary=`which knife`
 
 if [ "$chef_binary" == "" ] ; then
-    sudo apt-get update
-    sudo apt-get install chef
+    apt-get update
+    apt-get install chef
     echo "export PATH=/var/lib/gems/1.8:$PATH" >> ~/.bash_profile
     . ~/.bash_profile
 fi
+
+mkdir -p /tmp/chef-solo
+mv cookbook /tmp/chef-solo/
+mv chef-solo.rb /tmp/chef-solo/
+mv chef-solo.json /tmp/chef-solo/
+cd /tmp/chef-solo
+chef-solo -c chef-solo.rb -j chef-solo.json
