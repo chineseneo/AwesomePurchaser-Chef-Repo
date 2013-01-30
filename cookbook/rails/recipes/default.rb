24e3ec394348093f7b34527988dbefaa62507d84
Longkai Cao <lkcao@thoughtworks.com>
2013-01-30 18:00:47 +0800

    add dependency of sqlite3 dev to rails recipe

--- cookbook/rails/recipes/default.rb
+++ cookbook/rails/recipes/default.rb
@@ -1,6 +1,14 @@
 # install ruby on rails
 gem_package "rack" do
+  version "1.5.1"
+  action :remove
+end
+
+gem_package "rack" do
   version "1.4.0"
+  actioin :install
 end
 
 gem_package "rails"
+
+package "libsqlite3-dev"
