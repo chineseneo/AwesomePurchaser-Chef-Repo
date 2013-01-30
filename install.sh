dc4d1f6e0d3918e50f919ca7bee0e6c7d09610d0
Longkai Cao <lkcao@thoughtworks.com>
2013-01-30 17:25:25 +0800

    add chef install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+chef_binary=`which chef-solo`
+knife_binary=`which knife`
+
+if [ "$chef_binary" == "" ] ; then
+    sudo apt-get update
+    sudo apt-get install -y ruby ruby-dev make
+    sudo gem install --no-rdoc --no-ri chef
+    sudo apt-get install chef
+    echo "export PATH=/var/lib/gems/1.8:$PATH" >> ~/.bash_profile
+    . ~/.bash_profile
+fi
