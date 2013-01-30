77b30cb5166ab5712f3301a3ecb59693e2f74437
Longkai Cao <lkcao@thoughtworks.com>
2013-01-30 18:04:43 +0800

    move ruby installer to chef. chef could be installed from apt.

---
+++ cookbook/ruby/recipes/default.rb
@@ -0,0 +1,3 @@
+# Make sure we have all we need to compile ruby implementations:
+package "ruby"
+package "ruby-dev"
--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ knife_binary=`which knife`
 
 if [ "$chef_binary" == "" ] ; then
     sudo apt-get update
-    sudo apt-get install -y ruby ruby-dev make
-    sudo gem install --no-rdoc --no-ri chef
     sudo apt-get install chef
     echo "export PATH=/var/lib/gems/1.8:$PATH" >> ~/.bash_profile
     . ~/.bash_profile
